74ac88a8c4ebba72926ceb03c3b764816f0d2b9c
Insidium <ryanchallen@outlook.com>
2020-05-28 17:02:59 +1000

    Added post for adding students, added forver server run script

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// FOREVER RUN SERVER forever -c "./node_modules/.bin/nodemon —exitcrash -L" app.js
+
 // setup our web server using the HTTP module that comes with Node.js
 const http = require('http'); //node library to start web server
 
@@ -18,6 +20,37 @@ const server = http.createServer((req, res) => {
 	} else if (method === 'GET' && url === '/students') {
 		res.setHeader('Content-Type', 'application/json');
 		res.end(JSON.stringify(students));
+		//else post a student to list route
+	} else if (method === 'POST' && url === '/students') {
+		// Post request on '/students'
+		console.log('received a POST request');
+		res.setHeader('Content-Type', 'application/json');
+		// Handle getting data from the client request (on POST)
+		let data = []; // Used to collect chunks of data
+		req.on('data', (chunk) => {
+			// Make sure data was sent on the right url
+			if (url === '/students') {
+				// This event fires when we receive data in the request. The data comes in chunks
+				console.log(`Data chunk available: ${chunk}`);
+				// We need to parse the chunk, or we will store it as a stream object
+				data.push(JSON.parse(chunk));
+			}
+		});
+		req.on('end', () => {
+			// The end event signifies the end of the request, and therfore the end of the data stream
+			// We'll store any data we got from a post in our array, then send our response to the client
+			// If we got data (for a post), add it to our array of students
+			// In this case, we only expect to get a single chunk of data - just a student name to add to our array of students
+			if (data.length > 0) {
+				console.log('retrieved data', data[0]);
+				students.push(data[0].name);
+				// Send the stringified list of students we've constructed according to the route and method
+				res.statusCode = 201; //'created' status code
+				res.end(JSON.stringify(students));
+			}
+		});
+
+		// Handle data and send back updated student list with end
 	} else {
 		res.statusCode = 404; // status code for route not found
 		res.setHeader('Content-Type', 'text/plain');
