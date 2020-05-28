d52a8d7b09c75a4bfacbe1993f4ab23fc6646e78
Insidium <ryanchallen@outlook.com>
2020-05-28 15:08:22 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// setup our web server using the HTTP module that comes with Node.js
+const http = require('http'); //node library to start web server
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// callback function within the http library called createServer takes in key variables 'request' and 'response' to act on the server
+const server = http.createServer((req, res) => {
+	res.statusCode = 200; // status code
+	res.setHeader('Content-Type', 'text/plain'); // header
+	res.end('Hello World'); // body
+});
+
+// tell our server what IP address to use and which to port number to listen on
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
