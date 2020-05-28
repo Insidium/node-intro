f162f61c605ab08e40dfcc3b743a08909ab52a33
Insidium <ryanchallen@outlook.com>
2020-05-28 15:26:58 +1000

    commented createServer function and server listener

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const port = 3000;
 // callback function within the http library called createServer takes in key variables 'request' and 'response' to act on the server
 const server = http.createServer((req, res) => {
 	res.statusCode = 200; // status code
-	res.setHeader('Content-Type', 'text/plain'); // header
-	res.end('Hello World'); // body
+	res.setHeader('Content-Type', 'text/plain'); // set content type for header
+	res.end('Hello World'); // send off content to body
 });
 
 // tell our server what IP address to use and which to port number to listen on
 server.listen(port, hostname, () => {
+	//confirmation msg
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
