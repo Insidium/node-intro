90ff2637298cb3f3e627ac654e551e865f822829
Insidium <ryanchallen@outlook.com>
2020-05-28 15:54:54 +1000

    Added students array for data use, added deconstructed variable and logic for intro routes

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ const http = require('http'); //node library to start web server
 const hostname = '127.0.0.1';
 const port = 3000;
 
+let students = ['Carlie', 'Tony', 'Sarah', 'Amber', 'Nav', 'Ryan'];
+
 // callback function within the http library called createServer takes in key variables 'request' and 'response' to act on the server
 const server = http.createServer((req, res) => {
-	res.statusCode = 200; // status code
-	res.setHeader('Content-Type', 'text/plain'); // set content type for header
-	res.end('Hello World'); // send off content to body
+	//deconstructing variable for the commented actions below
+	const { method, url, header } = req;
+	//first route for home
+	if (method === 'GET' && url === '/') {
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('Matching Students');
+		//else get the students list route
+	} else if (method === 'GET' && url === '/students') {
+		res.setHeader('Content-Type', 'application/json');
+		res.end(JSON.stringify(students));
+	}
+	// res.statusCode = 200; // status code
+	// res.setHeader('Content-Type', 'text/plain'); // set content type for header
+	// res.end('Hello World'); // send off content to body
 });
 
 // tell our server what IP address to use and which to port number to listen on
